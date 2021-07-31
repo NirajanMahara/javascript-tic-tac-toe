b9aeec30a45d2e68162af03d7bbe765ff0c6fe46
Nirajan Mahara <mnirajan9@gmail.com>
2021-07-31 13:37:09 +0545

    🚀 Add `Game.js` javascript for game control and see some visualization

---
+++ js/Game.js
@@ -0,0 +1,19 @@
+export default class Game {
+  constructor() {
+    console.log('🎮 init Game 💥');
+    this.turn = 'X';
+    this.board = new Array(9).fill(null);
+  }
+
+  nextTurn() {
+    if (this.turn == 'X') {
+      this.turn = 'O';
+    } else {
+      this.turn = 'X';
+    }
+  }
+
+  makeMove(i) {
+    this.board[i] = this.turn;
+  }
+}
--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
+import Game from './Game.js';
+
+let game = new Game();
+
 console.log('🚀 JavaScript in console 💥');
+
+console.log(game.board);
+console.log('🐱‍👤 My turn is', game.turn);
+game.nextTurn();
+console.log('👽 My turn is', game.turn);
+game.makeMove(3);
+console.log(game.board);
+game.nextTurn();
+game.makeMove(8);
+console.log(game.board);
