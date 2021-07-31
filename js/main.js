afad442a38f05482fc76e0c7583641496bde51f4
Nirajan Mahara <mnirajan9@gmail.com>
2021-07-31 14:56:05 +0545

    🚀 Add `addEventListener` on `click` forEach `tile` on selected `.board__tile` of `index.html` and log

--- js/main.js
+++ js/main.js
@@ -4,18 +4,9 @@ import GameView from './GameView.js';
 let game = new Game();
 let gameView = new GameView();
 
-gameView.updateBoard(game);
-
-console.log('🚀 JavaScript in console 💥');
-
-console.log(game.board);
-console.log('🐱‍👤 My turn is', game.turn);
-game.nextTurn();
-console.log('👽 My turn is', game.turn);
-game.makeMove(3);
-console.log(game.board);
-game.nextTurn();
-game.makeMove(8);
-console.log(game.board);
-
-gameView.updateBoard(game);
+let tiles = document.querySelectorAll('.board__tile');
+tiles.forEach((tile) => {
+  tile.addEventListener('click', () => {
+    console.log('🖱 Tile clicked 💥');
+  });
+});
